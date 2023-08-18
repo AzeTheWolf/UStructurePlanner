e2c9cce91a1f0ccb3445a5cf3f87eb72b5e06fa2
Sariel <randomusrid@gmail.com>
2023-08-18 14:08:13 +0200

    Put version info in footer

--- src/frontend/components/Footer.svelte
+++ src/frontend/components/Footer.svelte
@@ -1,3 +1,9 @@
+<script>
+    let versions = {node: "-", electron: "-", chrome: "-", app: "-"};
+
+    globalThis.syslink.getElectronData().then(x => {versions = x})
+</script>
+
 <style>
     footer {
         background-color: var(--colBg);
@@ -5,9 +11,18 @@
         grid-column: 1 / 4;
         grid-row: 3;
         
-        font-size: 12px;
+        font-size: 11px;
+        line-height: 18px;
+        padding: 0 10px;
+
+        color: #888;
     }
 </style>
 
 <footer>
+    <div style="float: left">
+        <span>Structure Planner v{versions.app}</span>
+    </div>
+    <div style="float: right">node {versions.node} | electron {versions.electron} | chrome {versions.chrome}</div>
+    <div style="clear:both"></div>
 </footer>
