799df5d80dfe1fa632059f73798f5ece9251bf79
Sariel <randomusrid@gmail.com>
2023-08-22 17:27:17 +0200

    Viewport control icons

--- src/frontend/components/UIPanes/ToolbarPane.svelte
+++ src/frontend/components/UIPanes/ToolbarPane.svelte
@@ -1,11 +1,24 @@
+<script lang="ts">
+    import ToolTile from "../ToolTile.svelte";
+</script>
+
 <style>
     aside {
         background-color: var(--colBg);
         border-top: var(--borderPane);
         grid-column: 1;
-        grid-row: 1 / 3
+        grid-row: 1 / 3;
+
+        display: flex;
+        justify-content: top;
+        align-items: center;
+        flex-direction: column;
     }
 </style>
 
 <aside>
+    <ToolTile name="Rotate view horizontaly" src="icons/rotateHorizontal.png"/>
+    <ToolTile name="Rotate view vertically" src="icons/rotatevertical.png"/>
+    <ToolTile name="Rotate view" src="icons/rotateBoth.png"/>
+    <ToolTile name="Zoom" src="icons/zoom.png"/>
 </aside>
